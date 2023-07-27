d307070a960ffea495ea511e3386a8946cb20c54
huuson72 <huuson0702@gmail.com>
2023-07-27 18:44:07 +0700

    1

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+  introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+  introDesc: `Hỏi Nhung xíu`,
+  btnIntro: 'OK',
+  title: 'Nhung có thích t khôn? 😙',
+  
+  btnYes: 'Coó💖💖💖',
+  btnNo: '💔 Mơ à !💔 ',
+  question:'Lý do Nhung thích t :D 😊😊',
+  reasonPlaceholder: 'Lý do',
+  btnReply: 'Gởi ! 💌💌',
+  reply: 'Chắc tại đẹp trai á :D 😊😊',
+  mess: 'Biết mà, cũng thích Nhung nhiều lắm 🥰. Love you 300.000 😘😘',
+  messDesc: '💬💬 Nhắn tin thui 💬💬',
+  btnAccept: 'Ok lun 💖',
+  messLink: 'https://www.facebook.com/messages/t/100010712107446'
+}
